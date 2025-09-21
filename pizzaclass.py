4b791b9a9df19e8d6e8e5fa08e927c4b3c488424
M. Mujeer Asghar <sakerkhan004@gmail.com>
2025-09-21 23:59:21 +0500

    Update Restaurant.py

--- pizzaclass.py
+++ pizzaclass.py
@@ -1,18 +1,22 @@
-class pizza:
+class Restaurant:
     def __init__(self,name,price):
         self.name=name 
         self.price=price
-        
     def display(self):
         print(f"Pizza Name: {self.name}, Price: ${self.price}")
-    
-    
-class burger(pizza):
-    
+
+
+class pizza(Restaurant):
     def display(self):
         return super().display()
     
+class burger(Restaurant):
+    
+    def display(Restaurant):
+        return super().display()
+    
         
-class ColdDrink(pizza):
+class ColdDrink(Restaurant):
     def display(self):
+
         return super().display()
